5992708081e2ae4a2cb4945c706c3d88701504ba
Christopher Strøm <detoxte@gmail.com>
2022-09-02 09:25:29 +0200

    Hotfix: Automate launch with Docker and Compose
    Signed-off-by: Christopher Strøm <detoxte@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /opt/ros/melodic/setup.bash
-cd /home/vortex/sim_ws && source devel/setup.bash && roslaunch simulator_launch robosub_sim.launch
+cd /home/vortex/sim_ws && source devel/setup.bash && roslaunch simulator_launch robosub2022_sim.launch
